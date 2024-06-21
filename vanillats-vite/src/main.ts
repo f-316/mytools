74fde59200dffd80b39c0138cd0fdc8634d4b47f
f-316 <f_316_f@yahoo.co.jp>
2024-06-22 03:24:39 +0900

    2case

--- vanillats-vite/src/main.ts
+++ vanillats-vite/src/main.ts
@@ -1,4 +1,5 @@
 import './style.css';
+import { MyButton } from './MyButton.ts';
 import typescriptLogo from './typescript.svg';
 import viteLogo from '/vite.svg';
 import { setupCounter } from './counter.ts';
@@ -15,11 +16,34 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
+    <!-- case 1 -->
+    <my-button>
+      ボタン
+    </my-button>
+    <!-- case 2 -->
+    <div id="my-button">
+    </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `;
-let aiueo;
+
+const colors = ['red', 'blue', 'green'];
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
-console.log('hoge');
+
+// case 1
+customElements.define('my-button', MyButton);
+
+// case 2
+const myButton = new MyButton();
+myButton.setAttribute('text', 'hoge');
+myButton.setAttribute('color', 'blue');
+const myButtonTemp = document.querySelector('#my-button');
+myButtonTemp?.append(myButton);
+
+let index = 0;
+setInterval(() => {
+  myButton.setAttribute('color', colors[index++ % colors.length]);
+  myButton.setAttribute('text', `count: ${index}`);
+}, 1000);
